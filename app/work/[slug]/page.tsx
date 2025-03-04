6eda7e6d95497d0d2d55bc45d1690c90491b9bb6
Harry Baines <harryb0905@googlemail.com>
2025-03-04 22:00:56 +0000

    fix deployment error

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -63,7 +63,6 @@ export default async function Work(props: { params: tParams }) {
   return (
     <PostLayout
       title={post.metadata.title}
-      description={post.metadata.summary}
       date={formatDate(post.metadata.publishedAt)}
       readingTime="5 min read"
       image={post.metadata.image || '/images/work-default.jpg'}
